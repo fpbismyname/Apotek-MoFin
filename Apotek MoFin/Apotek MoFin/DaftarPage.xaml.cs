523a7f37739dc7af3733ccf4e2fb3a1fa45422a0
FPB <FPB@WIN-3DMUCKEVLCS>
2023-03-29 16:30:52 +0700

    Add project files.

---
+++ Apotek MoFin/Apotek MoFin/DaftarPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Apotek_MoFin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DaftarPage : ContentPage
+    {
+        public DaftarPage()
+        {
+            InitializeComponent();
+        }
+
+        private void login_tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        private void daftar_clicked(object sender, EventArgs e)
+        {
+
+            if (!username_daftar.Text.Equals(String.Empty) && password_daftar.Text.Equals(confirm_password_daftar.Text))
+            {
+                Navigation.PushAsync(new BerandaPage());
+            } else if (username_daftar.Text.Equals(String.Empty) && password_daftar.Text.Equals(confirm_password_daftar.Text))
+            {
+                DisplayAlert("Ops..","Mohon lengkapi username dan passwordnya","Okay");
+            }
+            else if (!username_daftar.Text.Equals(String.Empty) && !password_daftar.Text.Equals(confirm_password_daftar.Text))
+            {
+                DisplayAlert("Ops..","Mohon samakan password barunya","Okay");
+            }
+        }
+    }
+}
---
+++ Apotek MoFin/Apotek MoFin/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Apotek_MoFin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void daftar_tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DaftarPage());
+        }
+        private void login_clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BerandaPage());
+        }
+    }
+}
